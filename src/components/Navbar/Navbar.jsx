e83f861fa2a63d62de9c2f031f60c7a4c13dd374
pyaephyowinn <pyaephyowinn@gmail.com>
2023-01-20 08:55:55 +0630

    wip

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { AppBar, Box, Divider, Stack, Typography } from "@mui/material";
+
+import imgLogo from "@assets/img-logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Box>
+      <AppBar>app bar</AppBar>
+      <AppBar
+        color="default"
+        position="fixed"
+        sx={{ left: 0, height: "100vh", width: "fit-content" }}
+      >
+        <Box>
+          <Stack divider={<Divider />}>
+            <Box paddingX={4} paddingY={3}>
+              <img src={imgLogo} width={160} />
+            </Box>
+            <Box paddingX={2} paddingY={3}>category</Box>
+            <Box paddingX={2} paddingY={3}>genres</Box>
+          </Stack>
+        </Box>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Navbar;
